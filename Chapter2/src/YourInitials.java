b16dd2c2968723733806c8861337ffb3f56c3f80
16265010 <16265010@L815WD220003.edu.cbe.ab.ca>
2022-09-13 09:20:10 -0600

    9:20

--- Chapter2/src/YourInitials.java
+++ Chapter2/src/YourInitials.java
@@ -1,9 +1,10 @@
 
 public class YourInitials {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("\n      A         H    H\n     A A        H    H \n    AAAAA       HHHHHH \n   A     A      H    H \n  A       A  .  H    H");
+		
 	}
 
 }
