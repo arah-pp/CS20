ef48ed63360500ac0d67cb454914aec9d4552ed8
16265010 <16265010@L815WD220003.edu.cbe.ab.ca>
2022-09-13 09:25:35 -0600

    9:25

--- Chapter2/src/YourInitials.java
+++ Chapter2/src/YourInitials.java
@@ -3,8 +3,16 @@ public class YourInitials {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("\n      A         H    H\n     A A        H    H \n    AAAAA       HHHHHH \n   A     A      H    H \n  A       A  .  H    H");
 		
+		System.out.println("\n      A         H    H\n     A A        H    H \n    AAAAA       HHHHHH \n   A     A      H    H \n  A       A  .  H    H"); //Displaying text
+		
+
 	}
 
 }
+
+/* Screen Dump
+
+
+
+*/
