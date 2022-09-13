8a4502467bb9dba58bc661f4c9b173f6864234ea
16265010 <16265010@L815WD220003.edu.cbe.ab.ca>
2022-09-13 10:19:19 -0600

    10:19

--- Chapter2/src/BinaryNumber.java
+++ Chapter2/src/BinaryNumber.java
@@ -10,7 +10,27 @@ public class BinaryNumber {
 	public static void main(String[] args) 
 	{
 	
-		System.out.println("");
+		System.out.println("Decimal:       Binary:"); //Displaying text
+		System.out.println("      1             1\n" //Displaying text and next line
+				+ "      2            10\n" 
+				+ "      3            11\n" 
+				+ "      4           100\n" 
+				+ "      5           101\n" 
+				+ "      6           110\n" 
+				+ "      7           111\n"
+				+ "      8          1000\n"
+				+ "      9          1001\n"
+				+ "     10          1010\n"
+				+ "     11          1011\n"
+				+ "     12          1100\n"
+				+ "     13          1101\n"
+				+ "     14          1110\n"
+				+ "     15          1111\n"
+				+ "     16         10000\n"
+				+ "     17         10001\n"
+				+ "     18         10010\n"
+				+ "     19         10011\n"
+				+ "     20         10100"); //Displaying text and ending code
 
 	}
 
@@ -18,6 +38,26 @@ public class BinaryNumber {
 
 /*
 
-
+Decimal:       Binary:
+      1             1
+      2            10
+      3            11
+      4           100
+      5           101
+      6           110
+      7           111
+      8          1000
+      9          1001
+     10          1010
+     11          1011
+     12          1100
+     13          1101
+     14          1110
+     15          1111
+     16         10000
+     17         10001
+     18         10010
+     19         10011
+     20         10100
 
 */
