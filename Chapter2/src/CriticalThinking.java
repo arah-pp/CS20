bcdbc272a31f892618b8a5f484825d8c6c0a3598
16265010 <16265010@L815WD220003.edu.cbe.ab.ca>
2022-09-14 10:40:53 -0600

    10:40

---
+++ Chapter2/src/CriticalThinking.java
@@ -0,0 +1,28 @@
+/*
+Program: ${CriticalThinkingQuestions}.java          Date: ${9/14/22}
+Author: Ashton Hirdle
+School: CHHS
+Course: Computer Science 20
+ */
+public class CriticalThinking 
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		3. 
+		
+		4. 
+		
+		5. 
+		
+		6. 
+		
+		7. 
+		
+		12. 
+		*/
+
+	}
+
+}
